49e3853bb6eac599ede911504f6a5744a26edb69
Krishna Das <120264734+krishnaswgithub@users.noreply.github.com>
2023-06-30 19:30:07 +0530

    Delete Project-Airbus-main directory

--- Project-Airbus-main/src/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import SearchReducer from "./SearchSlice";
-
-export const store = configureStore({
-    reducer: {
-        search : SearchReducer,
-        
-    },
-})
